5402749c6b055519284b5f45f7784db69ca79715
cynicalish <aldarimel@gmail.com>
2015-08-17 23:25:17 -0700

    x89 fix

--- RaidCore/Encounters/GA/ExperimentX89.lua
+++ RaidCore/Encounters/GA/ExperimentX89.lua
@@ -126,7 +126,8 @@ end
 
 function mod:OnDebuffApplied(unitName, splId, unit)
     if splId == 47316 then
-		if unitName == GetPlayerUnit():GetName() then
+		local playerUnit = GameLib.GetPlayerUnit()
+		if unitName == playerUnit:GetName() then
         	core:AddMsg("LITTLEB", self.L["LITTLE BOMB on YOU !!!"], 5, "RunAway", "Blue")
 			core:AddBar("LITTLEB", self.L["LITTLE BOMB"], 5, 1)
 		end
@@ -140,7 +141,6 @@ function mod:OnUnitStateChanged(unit, bInCombat, sName)
             local playerUnit = GameLib.GetPlayerUnit()
             playerName = playerUnit:GetName()
             core:AddUnit(unit)
-            core:UnitDebuff(playerUnit)
             core:WatchUnit(unit)
             core:AddBar("KNOCKBACK", self.L["KNOCKBACK"], 6)
             core:AddBar("SHOCKWAVE", self.L["SHOCKWAVE"], 17)
